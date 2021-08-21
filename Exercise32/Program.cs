22c8ce8aeafd3913b93d7a89f92c72e4d483786c
Sean L <splambert1020@gmail.com>
2021-08-21 19:38:44 -0400

    initial commit

---
+++ Exercise32/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = new int[] { 2, 8, 0, 24, 51 };
+            string cont = "y";
+
+
+            while (cont == "y")
+            {
+                Console.WriteLine("Please enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+                int length = 5;
+                int numberIndex;
+                bool check = false;
+
+                for(int i = 0; i < length ; i++)
+                {
+                    if (number == numberArray[i])
+                    {
+                        numberIndex = Array.IndexOf(numberArray, number);
+                        Console.WriteLine($"The value {number} can be found at index {numberIndex}.");
+                        check = true;
+                        break;
+                    }                  
+                }
+                if (check == false)
+                {
+                    Console.WriteLine($"The value {number} is not in the array.");
+                }
+
+
+                Console.WriteLine("Would you like to continue? (y/n)");
+                cont = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ Exercises31-32/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercises31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = new int[] { 2, 8, 0, 24, 51 };
+            int arrayIndex;
+            string cont = "y";
+
+
+            while(cont == "y")
+            {
+                Console.WriteLine("Please enter an index of the array: ");
+                arrayIndex = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"The value at index {arrayIndex} is {numberArray.GetValue(arrayIndex)}");
+
+                Console.WriteLine("Would you like to continue? (y/n)");
+                cont = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
